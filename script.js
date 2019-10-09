3ae8d4e6e0171265010e2a9141f23d6ab06307c2
cis101 <cis101@secs.iub.edu.bd>
2019-10-09 09:19:13 +0600

    task2

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ var headingText = document.getElementById('heading')
 console.log(heading.textContent)
 
 /* Task 2 using plain JavaScript */
+//task2a
 function addText(event){
 var p = document.createElement('p')
 p.innerText = "Hello World"
+p.style.color = "red"
 var div = document.getElementById('task2a')
 div.appendChild(p)
-
 }
 
+//task2b
 
+function changebg1(event){
+    document.body.style.background = event.target.innerText.toLowerCase(); 
+}
+
+// function changebg2(event){
+//     document.body.style.background = "green" 
+// }
 
 /* Task 4 using jQuery */
