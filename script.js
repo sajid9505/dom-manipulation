58db57da8fbdb260cd0c6b826241114f36ca47ee
cis101 <cis101@secs.iub.edu.bd>
2019-10-09 08:59:07 +0600

    Task 2

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var headingText = document.getElementById('heading')
 console.log(heading.textContent)
 
 /* Task 2 using plain JavaScript */
+function addText(event){
+var p = document.createElement('p')
+p.innerText = "Hello World"
+var div = document.getElementById('task2a')
+div.appendChild(p)
+
+}
 
 
 
